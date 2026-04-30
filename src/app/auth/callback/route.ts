a6affa59ee3f714cde821242731568775b6dfec2
bmpsapin-creator <bmpsapin@gmail.com>
2026-04-30 19:53:55 +0200

    Fix formatting in route.ts

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -8,5 +8,5 @@ export async function GET(request: Request) {
     const supabase = createClient()
     await supabase.auth.exchangeCodeForSession(code)
   }
-  return NextResponse.redirect(\`\${origin}/dashboard\`)
+  return NextResponse.redirect(`${origin}/dashboard`)
 }
